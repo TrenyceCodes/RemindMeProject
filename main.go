e6d85bde04deaf248663d0990b5cd96a5bb33226
Neice <52137719+TrenyceCodes@users.noreply.github.com>
2024-05-16 01:29:11 -0400

    Setting up env and gitignore

--- main.go
+++ main.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	serverPort := os.Getenv("PORT")
+
 	server := gin.Default()
 
 	server.GET("/", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{"message": "hello world"})
 	})
 
-	if err := server.Run(":3001"); err != nil {
+	if err := server.Run(":" + serverPort); err != nil {
 		fmt.Println("Server is experiencing problems running")
 	}
 }
