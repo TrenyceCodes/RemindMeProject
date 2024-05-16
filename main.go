f56d9a3411167763187a36951d001552405ab230
Neice <52137719+TrenyceCodes@users.noreply.github.com>
2024-05-16 01:45:08 -0400

    Updating and refactoring code

--- main.go
+++ main.go
@@ -11,17 +11,23 @@ import (
 )
 
 func main() {
+	StartGinServer()
+}
+
+func StartGinServer() {
+	gin.SetMode(gin.ReleaseMode)
+
+	server := gin.New()
+
+	server.GET("/", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{"message": "hello world"})
+	})
+
 	if err := godotenv.Load(); err != nil {
 		log.Fatal(err)
 	}
 
 	serverPort := os.Getenv("PORT")
-
-	server := gin.Default()
-
-	server.GET("/", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{"message": "hello world"})
-	})
 
 	if err := server.Run(":" + serverPort); err != nil {
 		fmt.Println("Server is experiencing problems running")
